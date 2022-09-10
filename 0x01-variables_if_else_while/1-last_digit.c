9d7b02a364a924e692490f3c45217a674bf3c3ef
Gosplex <johngospel003@gmail.com>
2022-09-09 18:46:03 -0700

    Added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	/* your code goes there */
 if    (answer > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, answer);
+printf("Last digit of %d is %d and is greater than 5\n", n, answer);
 }
 else if    (answer == 0)
 {
-printf("Last digit of %d is %d and is 0", n, answer);
+printf("Last digit of %d is %d and is 0\n", n, answer);
 }
 else if    (answer < 6 && answer != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, answer);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, answer);
 }
 	return (0);
 }
