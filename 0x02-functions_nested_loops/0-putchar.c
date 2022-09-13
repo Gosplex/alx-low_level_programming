c39f686a198463b7c697f79b24e80cdbc659005b
Gosplex <johngospel003@gmail.com>
2022-09-12 23:12:20 -0700

    Added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes here */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char  i[20] = "_putchar";
+printf("%s\n", i);
+return (0);
+}
