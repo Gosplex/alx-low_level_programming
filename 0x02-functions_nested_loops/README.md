4e7e715d24bed6c7891b4d44c8e03359e38bcfeb
Gosplex <johngospel003@gmail.com>
2022-09-13 09:01:36 -0700

    Added

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Functions
+Functios
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * Header file contains all the functions used in
+ * 0x02-functions_nested_loop_directory
  */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+
+int _putchar(c)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
