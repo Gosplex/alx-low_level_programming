76c7465b14fe05a4d34732bdd12bbdff41993e12
Gosplex <johngospel003@gmail.com>
2022-09-13 13:37:41 -0700

    Added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - printts the alphabet in lowercase, followed by a new line.
+ * print_alphabet - printts the alphabet in lowercase, a new line.
  */
 void print_alphabet(void)
 {
 	char letter;
-
-	for	(letter = 'a';	letter <= 'z';	letter++)
-			{
+for (letter = 'a' letter <= 'z'; letter++)
+{
 				_putchar(letter);
 			}
 				_putchar('\n');
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,7 @@ int print_last_digit(int n)
 	{
 		last_digit *= -1;
 	}
-
-	_purchar(last_digit + '0');
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
