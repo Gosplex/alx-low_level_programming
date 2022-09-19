57318b6448bea8ec85960b306de81e52f5d344fe
Gosplex <johngospel003@gmail.com>
2022-09-19 01:00:46 -0700

    Pointers && Arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of two integers
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _strlen - Checks the length of a string
+ * @s: The string to be evaluated
+ *
+ * Return: Length of @s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+
+	return (0);
+}
